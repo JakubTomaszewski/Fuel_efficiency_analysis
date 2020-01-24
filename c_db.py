0338068e0af2441d485947174d2207ac8b0833a2
JakubTomaszewski <57564656+JakubTomaszewski@users.noreply.github.com>
2020-01-24 17:21:56 +0100

    gituwa

--- c_db.py
+++ c_db.py
@@ -1,24 +1,44 @@
-import sqlite3
-
-#creating db and a connection
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-
 #importing and preparing data to be inserted
 import pandas as pd
 
 fuel = pd.read_csv('https://assets.datacamp.com/production/repositories/516/datasets/2f3d8b2156d5669fb7e12137f1c2e979c3c9ce0b/automobiles.csv', index_col='yr', parse_dates=True)
+fuel = fuel.drop_duplicates(subset=['name']) #dropping duplicate cars
 prices = pd.read_csv('GASREGCOVW.csv', index_col=0, parse_dates=True)
-cars = fuel.loc[:,['name', 'origin']].drop_duplicates() #dropping duplicate values
+cars = fuel.loc[:,['name', 'origin']]
 cars = cars.set_index('name')
 spec = fuel.loc[:,['name', 'hp', 'accel', 'displ', 'mpg', 'weight']].set_index('name')
+
 prices = prices.rename(columns={'GASREGCOVW':'price_gallon'}, errors='raise')
-
-
 
 #resampling data by 6 months
 prices = prices.resample('6M').mean()
 prices['region'] = 'US'
 
-print(cars.head())
 
+#creating db and a connection
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+'''
+create tables in a db
+CARS with car models and their origin
+SPEC with the cars specification data (foreign key the 'name' with the cars table)
+FUEL with fuel cost data for each year/6months (resampled)
+'''
+
+c.execute('''CREATE TABLE Cars(
+            mark_model text PRIMARY KEY,
+            origin char(2)
+            );''')
+
+
+# conn.commit()
+#create spec db
+c.execute('''CREATE TABLE spec
+            hp real
+            accel real
+            distance real
+
+
+            ''')
